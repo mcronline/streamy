76a91a0c80e3ae9b989768b95203c78d97b8e32f
Michel Rozatti <contact@michelcurti.dev>
2019-11-25 10:20:59 -0300

    added StreamCreate action funcionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from "../api/streams";
+
 export const signIn = userId => {
   return {
     type: "SIGN_IN",
@@ -8,3 +10,7 @@ export const signIn = userId => {
 export const signOut = () => {
   return { type: "SIGN_OUT" };
 };
+
+export const createStream = formValues => async dispatch => {
+  streams.post('/streams', formValues);
+}
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -65,7 +67,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
