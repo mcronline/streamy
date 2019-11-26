9efa626725b648bed8ee12d57d6da88508b94473
Michel Rozatti <contact@michelcurti.dev>
2019-11-26 09:32:28 -0300

    Created a separete file for authReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,7 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import streamReducer from "./streamReducer";
-
-const INITIAL_USER_AUTH_STATUS = {
-  isSignedIn: null,
-  userId: null
-};
-
-const authReducer = (userAuthStatus = INITIAL_USER_AUTH_STATUS, action) => {
-  switch (action.type) {
-    case "SIGN_IN":
-      return { ...userAuthStatus, isSignedIn: true, userId: action.payload };
-
-    case "SIGN_OUT":
-      return { ...userAuthStatus, isSignedIn: false, userId: null };
-
-    default:
-      return userAuthStatus;
-  }
-};
+import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
